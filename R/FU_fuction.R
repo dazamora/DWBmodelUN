51331b17eae2fcb0d15c039c1f441a0b161fc414
dazamora <dazamoraa@unal.edu.co>
2019-01-22 16:41:25 -0500

    First modification

--- R/FU_fuction.R
+++ R/FU_fuction.R
@@ -1,4 +1,4 @@
-#' Title
+#' ESTA ES LA FUCION DE NICO
 #'
 #' @param numerador 
 #' @param denominador 
@@ -10,6 +10,8 @@
 #' @examples
 #' 
 fun_FU<-function(numerador,denominador,param){
-  F_FU=1+numerador/denominador-(1+(numerador/denominador)^(1/(1-param)))^(1-param)
+  F_FU <- 1 + numerador/denominador-(1+(numerador/denominador)^(1/(1-param)))^(1-param)
+  
+  X <- rnorm(100)
   return(F_FU)
 }
