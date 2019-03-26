302329295617d3b71b2129019b6ad917c9d01c34
nduqueg <nduqueg@unal.edu.co>
2019-03-26 18:05:28 -0500

    A function that uploads the forcings was programmed. It also contains the description of the function with its parameters and the values that returns

--- R/upload_forcings.R
+++ R/upload_forcings.R
@@ -1,18 +1,65 @@
-#' Cargar forzamientos
+#' @name 
+#' UpFrocing
 #'
-#' @param numerador 
-#' @param denominador 
-#' @param param 
+#' @title Upload Forcings
 #'
-#'@param precip
+#' @description This function loads the precipitation and evapotranspiration estimates that will be used
+#' to run or force the DWB model. It also creates a directory where stores csv files if the original forcing
+#' files are in raster format.
 #'
-#' @return
-#' @export
+#' @param path_p is a character string that specifies the directory where the precipitation rasters or
+#' the csv file are stored. The csv file must have nrows= N° of cells and ncol= N° of time steps.
+#' @param path_pet is a character string that specifies the location of the potential evapotranspiration rasters or
+#' the csv file are stored. The csv file must have nrows= N° of cells and ncol= N° of time steps.
+#' @param file_type Character string that specifies the forcing file formats, it should be "raster" or "csv",
+#' the default value is "raster".
+#' @param format Character string that specifies the format file of the Rasters, posible values are "GTiff"
+#' and "NCDF". Default value is "GTiff"
+#'
+#' @return a list containing the two Zoos (P and PET)
+#' 
+#' @author David Zamora <dazamoraa@unal.edu.co>
+#' Nicolas Duque Gardeazabal <nduqueg@unal.edu.co> 
+#' Water Resources Engineering Research Group - GIREH
 #'
 #' @examples
-upload_forcings<-function(numerador,denominador,param){
-  F_FU <- 1 + numerador/denominador-(1+(numerador/denominador)^(1/(1-param)))^(1-param)
+#' meteo <- UpForcing(path_p="./precip/", path_pet="./pet/", file_type="raster", format= "NCDF")
+#' meteo <- UpForcing(path_p="./precip/", path_pet="./pet/", file_type="csv")
+#' 
+UpForcing<-function(path_p="./precip/", path_pet="./pet/", file_type="raster", format= "GTiff"){
   
-  # recordar separar variables
-  return(F_FU)
+  if (file_type=="raster"){
+    # ---- identify raster format and loading----
+    if (format=="GTiff"){
+      
+      pet_files <- list.files(path_pet)
+      pet <- raster::stack(paste(path_pet,pet_files,sep = ""))
+      
+      p_files <- list.files(path_p)
+      p <- raster::stack(paste(path_p,p_files,sep = ""))
+    }else if(format=="NCDF"){
+      
+      pet <- raster::stack(path_pet)
+      p <- raster::stack(path_p)
+    }
+    
+    # ---- transformation to dataframes ----
+    p_v <- raster::rasterToPoints(p)[,-c(1,2)]
+    pet_v <- raster::rasterToPoints(pet)[,-c(1,2)]
+    
+    # ---- print forcings ----
+    dir.create("./forcings", showWarings=F)
+    write.csv(p_v, "./precip.csv")
+    write.csv(pet_v, "./pet.csv")
+    
+  }else {  # load forcings in csv files
+    pet_files <- list.files(path_pet)
+    p_files <- list.files(path_p)
+    
+    p_v <- read.csv(paste(path_p, p_files,sep = ""))
+    pet_v <- read.csv(paste(path_pet, pet_files,sep = ""))
+  }
+  
+  meteo <- list(pet <- pet_v, p <- p_v)
+  return(meteo)
 }
