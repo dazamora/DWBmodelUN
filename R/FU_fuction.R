abb4754d95c7c9937917cd0506236e95ee7f9467
nduqueg <nduqueg@unal.edu.co>
2019-04-28 15:47:00 -0500

    The function was commented and the condition that identifies in which positions it is an inconsistency with the denominator was added, this condition is later on defined with a value of 1

--- R/FU_fuction.R
+++ R/FU_fuction.R
@@ -1,16 +1,30 @@
-#' ESTA ES LA FUCION DE NICO
+#' @name 
+#' Fu's function
+#' 
+#' @title
+#' Fu's function for relationship between precipitation and evapotranspiration
 #'
-#' @param numerador 
-#' @param denominador 
-#' @param param 
+#' @param numerator is the variable which will be inserted as numerator in Fu's function. In Fu's original
+#' function, the variable used is the potential evapotranspiration. It can be a value or a numeric vector,
+#' in which case it must have the same length as the denominator vector.
+#' @param denominator is the variable which will be inserted as numerator in Fu's function. In Fu's original
+#' function, the variable used is the precipitation. It can be a value or a numeric vector,
+#' in which case it must have the same length as the numerator vector.
+#' @param param parameter of Fu model which controls the evapotrasnpiration efficiency, yet it is named depending
+#' on the variables used as numerator and denominator. It must be an unique value of type double.
 #'
-#' @return
-#' @export
+#' @return a value or a vector (depending on which kind of data was introduced for numerator and denominator)
 #'
 #' @examples
+#' PET <- 1000
+#' P <- 2000
+#' alpha <- 0.69  # value used by Zhang et al. (2008) to study the mean anual actual evapotranspiration as a function of Potential ET and Precipitation
+#' fun_FU(PET, P, alpha)
 #' 
-fun_FU<-function(numerador,denominador,param){
-  F_FU <- 1 + numerador/denominador-(1+(numerador/denominador)^(1/(1-param)))^(1-param)
+funFU<-function(numerator,denominator,param){
+  zerovalues<-which(denominator==0)
+  F_FU <- 1 + numerator/denominator-(1+(numerator/denominator)^(1/(1-param)))^(1-param)
+  F_FU[zerovalues]<-1
   
   # recordar separar variables
   return(F_FU)
