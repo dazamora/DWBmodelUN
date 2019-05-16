0c1b139375929fff05bddb2a75e987d46f0426bc
pfarboledao <pfarboledao@unal.edu.co>
2019-05-15 22:33:00 -0500

    First follow-up over Coord_comp function. Everything was translated to english, and a new if condition was add. Also, a more descriptive info was written. This function still needs further revision and improvement

--- R/Coord_comparison.R
+++ R/Coord_comparison.R
@@ -1,22 +1,40 @@
-#' Title
-#'
+#' @name 
+#' Raster coordinates comparison
+#' 
+#' @Title
+#' Raster coordinates comparison
+#' 
+#' @description This function compares three characteristicas from two rasters: 
+#' coordinates, resolution, and number of layers (if the rasters have more than one) 
+#' from two different rasters stacks, and let to know if they are using the same geographical info.,
+#' or if new set-up should be done
+#' 
 #' @param r1 
 #' @param r2 
 #'
-#' @return
+#' @return It prints to console if the two rasters are on the same coordinates or not, and return
+#' a boolean, TRUE if the rasters are on the same coordinates, and FALSE if not
 #' @export
 #'
+#' @author 
+#' Nicolas Duque Gardeazabal <nduqueg@unal.edu.co> 
+#' Pedro Felipe Arboleda Obando <pfarboledao@unal.edu.co>
+#' David Andres Zamora Avila<dazamoraa@unal.edu.co>
+#' Carolina Vega Viviescas <cvegav@unal.edu.co>
+#' Water Resources Engineering Research Group - GIREH
+#' Universidad Nacional de Colombia - sede Bogotá
 #' @examples
 #' 
 Coord_comparison<-function(r1, r2){
   er1<-extent(r1)
   er2<-extent(r2)
   if(er1==er2){
-    print("Extensi?n verificada")
+    print("Coordinates verified")
     if(res(r1)[1]==res(r2)[1]&res(r1)[2]==res(r2)[2]){
-      print("Resoluci?n verificada")
-      if (nlayers(r1)==nlayers(r2)){
-        print("Longitud verificada")
+      print("Resolution verified")
+      if (nlayers(r1)!=1 & nlayers(r2)!=1)
+        if (nlayers(r1)==nlayers(r2)){
+        print("Number of layers verified")
         return(TRUE)
         #if(sum(!is.na(r1[[1]]))==sum(!is.na(r2[[1]]))){
         #print("Celdas con valor verificadas")
@@ -26,18 +44,18 @@ Coord_comparison<-function(r1, r2){
         #return(FALSE)
         #}
         
-      }else{
-        print("Verificar la longitud de las series")
+        }else{
+        print("Please verify number of layers")
         return(FALSE)
       }
       
     }else{
-      print("Info. geogr?fica no est? en la misma resoluci?n")
+      print("Please verify raster resolution")
       return(FALSE)
     }
     
   }else{
-    print("Informaci?n geogr?fica no est? en las mismas coordenadas")
+    print("Please verify raster coordinates")
     
   }
 }
